e261bb2b44febaa95632c1c674e44f2f4e56a03f
Narasimha Badrinath <bnarasimha21@gmail.com>
2024-07-05 16:56:40 +0530

    Added refine image functionality

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import gradio as gr
 from diffusers import DiffusionPipeline
 from huggingface_hub import HfApi
 from openai import OpenAI
+from diffusers import StableDiffusionXLImg2ImgPipeline
+from diffusers.utils import load_image
+
 
 huggingfaceApKey = os.environ["HUGGINGFACE_API_KEY"]
 hugging_face_user = os.getenv("HUGGING_FACE_USERNAME")
@@ -25,10 +28,10 @@ def improve_prompt(prompt):
     client = OpenAI(api_key = os.environ["OPENAI_API_KEY"])
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
-        max_tokens=150,
+        max_tokens=77,
         messages=[
             {"role": "system", "content": "You are a room interiar designer"},
-            {"role": "user", "content": f"Generate a description for a room based on the following input. Keep it under 5 sentenses: {prompt}"}
+            {"role": "user", "content": f"Generate a description for a room based on the following input. Keep it under 3 sentenses: {prompt}"}
         ]
     )
     ai_generated_prompt = completion.choices[0].message.content
@@ -58,7 +61,23 @@ def generate_image(user_prompt, use_ai_prompt, ai_generated_prompt, selected_mod
     pipe = pipe.to("cuda" if torch.cuda.is_available() else "cpu")
 
     image = pipe(prompt).images[0]
+    output_path = "ui_screenshot/ai_generated_image.png"
+    image.save(output_path)
     return image
+
+
+def refine_generated_image(generated_image_output):        
+    pipe = StableDiffusionXLImg2ImgPipeline.from_pretrained(
+        "stabilityai/stable-diffusion-xl-refiner-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
+    )
+    pipe = pipe.to("cuda" if torch.cuda.is_available() else "cpu")
+    url = "ui_screenshot/ai_generated_image.png"
+
+    input_image = load_image(url).convert("RGB")
+    prompt = ""
+    refined_image = pipe(prompt, image=input_image).images[0]
+    return refined_image
+
 
 models = getHuggingfaceModels()
 
@@ -76,10 +95,21 @@ with gr.Blocks() as demo:
         with gr.Column():
             ai_generated_prompt = gr.Textbox(label="AI generated detailed prompt")
 
-    generated_image_output = gr.Image(label="Generated Image", width=512, height=512)
+    generated_image_output = gr.Image(label="Generated Image", sources="clipboard", width=512, height=512)
+
+    with gr.Row():
+        with gr.Column():
+            refine_image = gr.Button(value="Refine Image")
+        with gr.Column():
+            ""
+        with gr.Column():
+            ""
+            
+    refine_image_output = gr.Image(label="Refined Image", width=512, height=512)    
 
     user_prompt.submit(fn=generate_ai_prompt, inputs=[user_prompt, use_ai_prompt, ai_generated_prompt], outputs=[ai_generated_prompt])
     ai_generated_prompt.change(fn=generate_image, inputs=[user_prompt, use_ai_prompt, ai_generated_prompt, model_list], outputs=[generated_image_output])
+    refine_image.click(fn=refine_generated_image, inputs=[generated_image_output], outputs=[refine_image_output])
 
 def launch():
     demo.launch()
